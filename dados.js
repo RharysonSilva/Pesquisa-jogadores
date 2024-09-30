965f59c518260d535b606b085b2ced6b4fb44e34
RharysonSilva <rharysonsilva@gmail.com>
2024-09-30 10:33:01 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo:"Cristiano Ronaldo",
+        descricao:"Cristiano Ronaldo é considerado o jogador mais completo da história",
+        link:"https://pt.wikipedia.org/wiki/Cristiano_Ronaldo",
+        tags:"maior cr7 gols melhor"
+    },
+    {
+        titulo:"Flavio Caça Rato",
+        descricao:"Flavio Caça Rato é considerado um dos homens mais lindos da terra",
+        link:"https://pt.wikipedia.org/wiki/Fl%C3%A1vio_Ca%C3%A7a-Rato",
+        tags:"bonito rato ratinho beleza lindo"
+    },
+
+    {
+        titulo:"Neymar",
+        descricao:"Neymar é considerado um dos homens mais fieis do mundo",
+        link:"https://pt.wikipedia.org/wiki/Neymar",
+        tags:"queda dribles brasileiro amantes fiel gustavo lima dança"
+    }
+
+];
